aaee720cdf1717b25aa051d75b8a1dc83da17f09
Nattapat Sukpootanan <Nattapat@Nattapats-Macbook.local>
2016-09-15 13:08:04 +0700

    test login component

---
+++ ui/components/Input.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export default class Input extends Component {
+  render() {
+    return (
+      <div className="Input">
+        <span className={this.props.icon}></span>
+        <input
+          id={this.props.name}
+          autoComplete="false"
+          required
+          type={this.props.type}
+          placeholder={this.props.placeholder}
+          />
+        {/* <label htmlFor={this.props.name}></label> */}
+      </div>
+    )
+  }
+}
