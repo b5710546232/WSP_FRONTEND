0b746e310c66b0e155df7c9920101ddb10f575d1
Nattapat Sukpootanan <Nattapat@Nattapats-Macbook.local>
2016-09-21 17:47:13 +0700

    changed name  ui/ path --> src/

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const autoprefixer = require('autoprefixer');
 module.exports = {
   devtool: 'eval-cheap-module-source-map',
   entry: [
-    './ui/index.js'
+    './src/index.js'
   ],
   output: {
     publicPath: "/static/",
@@ -14,26 +14,6 @@ module.exports = {
   },
   module: {
     loaders:[
-      // {
-      //   test: /\.js$/,
-      //   loaders: ['babel?cacheDirectory'],
-      //   exclude: /(node_modules|bower_components)/,
-      // },
-      // {
-      //   test: /\.css$/,
-      //   loaders: ['style', 'css?module&localIdentName=[local]___[hash:base64:5]', 'postcss'],
-      // },
-      // {
-      //   test: /\.scss$/,
-      //   loaders: ['style', 'css?module&localIdentName=[local]___[hash:base64:5]', 'postcss', 'sass'],
-      // },
-      // {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      // {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      // {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      // {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      // {test: /\.html$/, loader: 'raw',exclude: /node_modules/},
-      // {test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,loader : 'file-loader'},
-      // { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
       {test: /\.js$/,loaders: ['babel?cacheDirectory'],exclude: /(node_modules|bower_components)/,},
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
@@ -42,9 +22,6 @@ module.exports = {
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
       { test: /\.json$/, loader: 'json-loader'},
       {test: /\.scss$/,loaders: ['style', 'css', 'sass','postcss']}
-      // { test: /\.scss$/, exclude: /node_modules/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap&includePaths[]=node_modules/compass-mixins/lib'},
-      // { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240'}
-
     ]
   },
   plugins: [
