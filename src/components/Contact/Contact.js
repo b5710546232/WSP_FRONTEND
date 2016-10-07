7f95e3594c0b70cec15f96e180f99126f29ed597
poom <poom10510@gmail.com>
2016-10-07 16:47:14 +0700

    modify Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,38 +11,25 @@ export default class ContactForm extends Component {
       return(
         <div className="text-center">
           <form className="ContactForm">
-          <div>
-            <div style={{width: 100}}>
-              <label htmlFor="">Namsailaiyen</label>
+            <div class="span2">
+              <h2>Namsailaiyen</h2>
             </div>
-          </div>
-          <br/>
-          <div>
-            <div style={{width: 100}}>
-              <label htmlFor="">E-mail: namsailaiyen@gmail.com</label>
+            <div class="span2">
+              <h4>E-mail: namsailaiyen@gmail.com</h4>
             </div>
-          </div>
-          <br/>
-          <div>
-            <div style={{width: 100}}>
-              <label htmlFor="">Phone number: (+66)80-000-0000</label>
+            <div class="span2">
+              <h4>Phone number: (+66)80-000-0000</h4>
             </div>
-          </div>
-          <br/>
-          <div>
-            <div style={{width: 100}}>
-              <label htmlFor="">Address</label>
+            <div class="span2">
+            <h4>Address</h4>
             </div>
-            <br/>
-            <div style={{width: 100}}>
-              <label htmlFor="">999/999 Bangkhen</label>
+            <div class="span2">
+                <h5>&nbsp&nbsp999/999 Bangkhen</h5>
+                <h5>&nbsp&nbsp Bangna Bangkok, Thailand 11150</h5>
             </div>
-            <br/>
-            <div style={{width: 100}}>
-              <label htmlFor="">Bangna Bangkok, Thailand 11150</label>
+            <div class="span2">
+              <img className="img-responsive center-block" width={800} height={600} src="src/assets/3.jpg"/>
             </div>
-          </div>
-          <br/>
           </form>
         </div>
       )
