07b1a32502d55a003fbbf0f99b8b00018b8343c2
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-21 22:19:07 +0700

    Initialize Experiment

---
+++ css-experiment-branch/app/index.js
@@ -0,0 +1,6 @@
+// path : /ui/index.js
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import routes from './routes'
+
+render(routes(), document.getElementById('app'))
