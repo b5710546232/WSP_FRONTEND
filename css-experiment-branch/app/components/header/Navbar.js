08140b30eb3ae7517dd9db9b0af73c613f16e78d
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-22 20:59:11 +0700

    Go go materialize

--- css-experiment-branch/app/components/header/Navbar.js
+++ css-experiment-branch/app/components/header/Navbar.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
-
+import '../../../dist/scss/navbar.scss'
 export default class Navbar extends Component {
   render() {
     return (
-      <h1>Nav Bar Here</h1>
+      <nav className="navbar navbar-fixed-top navbar-light bg-primary" >
+        <button className="navbar-toggler hidden-lg-up " type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div className="collapse navbar-toggleable-md" id="navbarResponsive">
+          <ul className="nav navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link " href="#">Nature Drink</a>
+            </li>
+            
+            <li className="nav-item ">
+              <a className="nav-link" href="#">Store<span className="sr-only">(current)</span></a>
+            </li>
+
+            <li className="nav-item ">
+              <a className="nav-link" href="#">Design</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
--- css-experiment-branch/app/components/home/slide/Slide.js
+++ css-experiment-branch/app/components/home/slide/Slide.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
-
+import '../../../../dist/scss/navbar.scss'
 export default class Slide extends Component {
   render() {
     return (
-      <h1>Slide Here</h1>
+        <div id="main-slide" className="carousel slide height" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#main-slide" data-slide-to="0" className="active"></li>
+            <li data-target="#main-slide" data-slide-to="1"></li>
+            <li data-target="#main-slide" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active">
+              <img className="auto" src="media/images/store.jpg" alt="Store"/>
+            </div>
+            <div className="carousel-item">
+              <img className="auto" src="media/images/design.jpg" alt="Design"/>
+            </div>
+            <div className="carousel-item">
+              <img className="auto" src="media/images/top product.jpg" alt="Top"/>
+            </div>
+          </div>
+          <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span className="icon-prev" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span className="icon-next" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
     )
   }
 }
