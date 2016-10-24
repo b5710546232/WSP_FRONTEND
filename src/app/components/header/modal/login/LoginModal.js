dfd039c42fda218b95749b8fce20b33820c05f09
b5710546232 <nattapat.su@ku.th>
2016-10-24 17:49:11 +0700

    problem with Input

--- src/app/components/header/modal/login/LoginModal.js
+++ src/app/components/header/modal/login/LoginModal.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import {Modal,Button,Input,Row} from 'react-materialize'
+import AuthedAction from '../../../../actions/AuthedAction'
 export default class LoginModal extends Component {
+  onLogin(e){
+    e.preventDefault()
+    let username = ReactDOM.findDOMNode(this.refs.username).value
+    let password = ReactDOM.findDOMNode(this.refs.password).value
+    console.log(ReactDOM.findDOMNode(this.refs.username).value);
+
+    let data = {
+      username: username,
+      password: password
+    }
+    // this.props.login(data)
+
+    ReactDOM.findDOMNode(this.refs.password).value = ''
+    ReactDOM.findDOMNode(this.refs.password).value.value = ''
+  }
   render() {
     return (
       <Modal
@@ -10,9 +27,13 @@ export default class LoginModal extends Component {
         }>
         <form>
           <Row className="center">
-            <Input name="username" s={12} label="Username" />
-            <Input name="password" type="password" label="password" s={12} />
-            <Button waves="light">Login</Button>
+            <Input name="username" s={12} label="Username"
+              ref = "username"
+               />
+            <Input name="password" type="password" label="password" s={12}
+              ref = "password"
+               />
+            <Button waves="light" type="submit" onClick={(e)=>this.onLogin(e)}>Login</Button>
           </Row>
         </form>
       </Modal>
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,6 +2,5 @@ import { combineReducers } from 'redux'
 import authed from './authed'
 import categories from './categories'
 import { routerReducer } from 'react-router-redux'
-console.log('routerReducer',routerReducer);
 const rootReducer = combineReducers({authed,routing: routerReducer,categories})
 export default rootReducer
