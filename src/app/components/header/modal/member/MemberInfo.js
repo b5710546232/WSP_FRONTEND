931f491029fad6e44c74e799677fd45ae590d4aa
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-25 02:54:49 +0700

    Edit user info without action

--- src/app/components/header/modal/member/MemberInfo.js
+++ src/app/components/header/modal/member/MemberInfo.js
@@ -5,10 +5,23 @@ class MemberInfo extends Component {
     super(props);
     this.state = {is_edit:false};
     this.handleEdit = this.changeState.bind(this,true)
-    this.handleSave = this.changeState.bind(this,false)
+    this.handleSave = this.onSave.bind(this)
   }
   changeState(value){
     this.setState({is_edit:value})
+  }
+  onSave(e) {
+    e.preventDefault()
+    let first_name = this.refs.form.first_name.value
+    let last_name = this.refs.form.last_name.value
+    let email = this.refs.form.email.value
+    let data = {
+      first_name: first_name,
+      last_name: last_name,
+      email : email
+    }
+    this.changeState(false)
+    this.props.onSave(data)
   }
   render(){
     return (
@@ -23,16 +36,16 @@ class MemberInfo extends Component {
             <Row>
               <Col s={12} ><span className="medium">Email : </span>{this.props.email}</Col>
             </Row>
-            <Button waves="light" onClick={this.handleEdit}>Edit</Button>
+            <Row><Col s={12}><Button waves="light" onClick={this.handleEdit}>Edit</Button></Col></Row>
           </div> :
-          <div>
+          <form ref="form">
             <Row>
-              <Input type="text" label="Firstname" defaultValue={this.props.first_name} s={12} m={6}></Input>
-              <Input type="text" label="Lastname" defaultValue={this.props.last_name} s={12} m={6}></Input>
+              <Input type="text" name="first_name" label="Firstname" defaultValue={this.props.first_name} s={12} m={6} ref="first_name"></Input>
+              <Input type="text" name="last_name" label="Lastname" defaultValue={this.props.last_name} s={12} m={6} ref="last_name"></Input>
             </Row>
-            <Row><Input type="email" label="Email" defaultValue={this.props.email} s={12} ></Input></Row>
-            <Button waves="light" onClick={this.handleSave}>Save</Button>
-          </div>
+            <Row><Input type="email" name="email" label="Email" defaultValue={this.props.email} s={12} ref="email"></Input></Row>
+            <Row><Col s={12}><Button waves="light" onClick={(e)=>this.onSave(e)}>Save</Button></Col></Row>
+          </form>
         }
       </div>
     )
