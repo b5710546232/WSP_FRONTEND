1e1fd04439f842d9063ca34304cba597c2c3c2e8
gunhappy <gunhappy123@gmail.com>
2016-10-25 14:47:54 +0700

    insert map

--- src/app/components/footer/AppFooter.js
+++ src/app/components/footer/AppFooter.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router'
 import {Footer,Row,Button} from 'react-materialize'
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps'
+
+const coords = {
+  lat: 13.846977,
+  lng: 100.5698995
+};
+
 export default class AppFooter extends Component {
+
+  onMapCreated(map) {
+   map.setOptions({
+     disableDefaultUI: true
+   });
+  }
+
   render() {
     return (
       <Footer copyrights="&copy; 2016 Copywrong Powered by Star Burst Stream"
@@ -20,6 +34,28 @@ export default class AppFooter extends Component {
             Company Here
             Address Here
           </p>
+          <div className='map_container'>
+          <Gmaps
+              width={'800px'}
+              height={'300px'}
+              lat={coords.lat}
+              lng={coords.lng}
+              zoom={15}
+              loadingMessage={'Map loading...'}
+              params={{v: '3.exp', key: 'AIzaSyD3FLaVBGkmNU0V79OcpMzLFmts_Hba1qU'}}
+              onMapCreated={this.onMapCreated}>
+            <Marker
+              lat={coords.lat}
+              lng={coords.lng}
+              draggable={true}
+              onDragEnd={this.onDragEnd} />
+            <InfoWindow
+              lat={coords.lat}
+              lng={coords.lng}
+              content={'Wellcome to Nature Drink'}
+              onCloseClick={this.onCloseClick} />
+          </Gmaps>
+          </div>
           <Row>
             <Button floating  className='red' waves='light' icon='add' />
             <Button floating  className='red' waves='light' icon='add' />
