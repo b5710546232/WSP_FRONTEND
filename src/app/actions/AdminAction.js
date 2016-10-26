b57174caa89016acc8d0eaa731b5a3b6b76d1b46
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-26 23:33:12 +0700

    ADMIN USER ACTION

---
+++ src/app/actions/AdminAction.js
@@ -0,0 +1,72 @@
+import {Action} from '../constants';
+import { CALL_API } from 'redux-api-middleware'
+import {ADMIN_USER_ENDPOINT} from '../constants/endpoints'
+
+export const loadUser = (id,token) => (
+  {[CALL_API]: {
+    endpoint: ADMIN_USER_ENDPOINT+id+'/',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization':'Token '+token
+    },
+    method: 'GET',
+    types: ['LOAD_ADMIN_USER_REQUEST', 'LOAD_ADMIN_USER_SUCCESS', 'LOAD_ADMIN_USER_FAILURE']
+  }}
+)
+
+export const createUser = (data) => (
+  (dispatch) =>
+    dispatch({
+      [CALL_API]: {
+        endpoint: ADMIN_USER_ENDPOINT,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+        types: [
+          'CREATE_USER_ADMIN_REQUEST',
+          {
+            type: 'CREATE_USER_ADMIN_SUCCESS',
+            payload: (_action, _state, res) => {
+              return res.json().then((data) => {
+                return data
+              })
+            }
+          },
+          'CREATE_USER_ADMIN_FAILURE'
+        ]
+      }
+    }
+  )
+)
+
+export const updateUser = (data) => (
+  (dispatch) =>
+    dispatch({
+      [CALL_API]: {
+        endpoint: ADMIN_USER_ENDPOINT,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'PUT',
+        body: JSON.stringify(data),
+        types: [
+          'UPDATE_USER_ADMIN_REQUEST',
+          {
+            type: 'UPDATE_USER_ADMIN_SUCCESS',
+            payload: (_action, _state, res) => {
+              return res.json().then((data) => {
+                return data
+              })
+            }
+          },
+          'UPDATE_USER_ADMIN_FAILURE'
+        ]
+      }
+    }
+  )
+)
