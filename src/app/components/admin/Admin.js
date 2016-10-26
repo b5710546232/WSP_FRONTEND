79ec7f866d313345a43b2f049f5a9b9c4c8f6b2c
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-26 21:19:49 +0700

    ADmin fix bug

--- src/app/components/admin/Admin.js
+++ src/app/components/admin/Admin.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
-export default class Admin extends Component {
+import {check_admin} from '../../actions/UserAction'
+import {connect} from 'react-redux'
+class Admin extends Component {
+  componentDidMount(){
+    this.props.check_admin(localStorage.token)
+  }
   render() {
     return (
       <div>
-        {this.props.user.is_admin
+        {this.props.user.is_admin?
           <div>OK you are admin</div>:
           <div>Permission Denied</div>
         }
@@ -11,3 +16,14 @@ export default class Admin extends Component {
     )
   }
 }
+const mapStateToProps = (state) => {
+  return state
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    check_admin: (token)=>{
+      dispatch(check_admin(token))
+    }
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Admin)
