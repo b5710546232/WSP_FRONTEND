e5bd6ba76081851a0e2080e7ee9bdf6d5727463c
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-26 23:53:56 +0700

    Address Action

--- src/app/actions/AdminAction.js
+++ src/app/actions/AdminAction.js
@@ -9,9 +9,9 @@ import {
   ADMIN_ITEMLINE_ENDPOINT
 } from '../constants/endpoints'
 
-export const loadUser = (id,token) => (
+export const loadUser = (token) => (
   {[CALL_API]: {
-    endpoint: ADMIN_USER_ENDPOINT+id+'/',
+    endpoint: ADMIN_USER_ENDPOINT,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
@@ -22,14 +22,15 @@ export const loadUser = (id,token) => (
   }}
 )
 
-export const createUser = (data) => (
+export const createUser = (data,token) => (
   (dispatch) =>
     dispatch({
       [CALL_API]: {
         endpoint: ADMIN_USER_ENDPOINT,
         headers: {
           'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization':'Token '+token
         },
         method: 'POST',
         body: JSON.stringify(data),
@@ -50,14 +51,15 @@ export const createUser = (data) => (
   )
 )
 
-export const updateUser = (data) => (
+export const updateUser = (id,data,token) => (
   (dispatch) =>
     dispatch({
       [CALL_API]: {
-        endpoint: ADMIN_USER_ENDPOINT,
+        endpoint: ADMIN_USER_ENDPOINT+id+'/',
         headers: {
           'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization':'Token '+token
         },
         method: 'PUT',
         body: JSON.stringify(data),
@@ -72,6 +74,76 @@ export const updateUser = (data) => (
             }
           },
           'UPDATE_USER_ADMIN_FAILURE'
+        ]
+      }
+    }
+  )
+)
+export const loadAddress = (id,token) => (
+  {[CALL_API]: {
+    endpoint: ADMIN_ADDRESS_ENDPOINT,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization':'Token '+token
+    },
+    method: 'GET',
+    types: ['LOAD_ADMIN_ADDRESS_REQUEST', 'LOAD_ADMIN_ADDRESS_SUCCESS', 'LOAD_ADMIN_ADDRESS_FAILURE']
+  }}
+)
+
+export const createAddress = (data,token) => (
+  (dispatch) =>
+    dispatch({
+      [CALL_API]: {
+        endpoint: ADMIN_ADDRESS_ENDPOINT,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization':'Token '+token
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+        types: [
+          'CREATE_ADDRESS_ADMIN_REQUEST',
+          {
+            type: 'CREATE_ADDRESS_ADMIN_SUCCESS',
+            payload: (_action, _state, res) => {
+              return res.json().then((data) => {
+                return data
+              })
+            }
+          },
+          'CREATE_ADDRESS_ADMIN_FAILURE'
+        ]
+      }
+    }
+  )
+)
+
+export const updateAddress = (id,data,token) => (
+  (dispatch) =>
+    dispatch({
+      [CALL_API]: {
+        endpoint: ADMIN_ADDRESS_ENDPOINT+id+'/',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization':'Token '+token
+        },
+        method: 'PUT',
+        body: JSON.stringify(data),
+        types: [
+          'UPDATE_ADDRESS_ADMIN_REQUEST',
+          {
+            type: 'UPDATE_ADDRESS_ADMIN_SUCCESS',
+            payload: (_action, _state, res) => {
+              return res.json().then((data) => {
+                return data
+              })
+            }
+          },
+          'UPDATE_ADDRESS_ADMIN_FAILURE'
         ]
       }
     }
