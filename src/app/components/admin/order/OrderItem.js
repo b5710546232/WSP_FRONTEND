5cc30f0f99b37e6b435d39abd81808ac1d562736
paiizz <wanchanapon.t@ku.th>
2016-10-29 18:48:04 +0700

    edit logic OrderItem

--- src/app/components/admin/order/OrderItem.js
+++ src/app/components/admin/order/OrderItem.js
@@ -8,40 +8,31 @@ class OrderItem extends Component {
     super(props)
   }
 
-  getUserbyID(id){
-    let usertemp = {id:'',first_name:'',last_name:'',email:'',username:'',is_staff:false,is_active:false}
-    this.props.admin.user.forEach((user)=>{
-      if(user.id === id)
-      usertemp = user
+  getAddressbyID(id){
+    let addresstemp = {address_number:'',country:'',distinct:'',id:'',username:'',province:'',is_active:false,road:'',sub_distinct:'',user:'',village:'',zipcode:''}
+    this.props.admin.address.forEach((address)=>{
+      if(address.user === id)
+      addresstemp = address
     })
-    return usertemp
+    return addresstemp
   }
 
-  // getAddressbyID(id){
-  //   let addresstemp = {address_number:'',country:'',distinct:'',id:'',username:'',province:'',is_active:false,road:'',sub_distinct:'',user:'',village:'',zipcode:''}
-  //   this.props.admin.address.forEach((address)=>{
-  //     if(address.user === id)
-  //     addresstemp = address
-  //   })
-  //   return addresstemp
-  // }
-
   getPaymentbyID(id){
-    let paymenttemp = {id:'',country:'',type:'',id:'',name:'',is_active:false}
+    let paymenttemp = {id:'',country:'',type:'',name:'',is_active:false}
     this.props.admin.method.forEach((payment)=>{
       if(payment.id === id)
       paymenttemp = payment
     })
     return paymenttemp
   }
+
   render() {
-    let itemList = this.props.itemlines.filter(itemline=>itemline.order===this.props.order.id)
-    let address = this.props.address.find(select_address=>select_address.id===this.props.order.address)
+    let Listitem = this.props.admin.item_line.filter((itemline)=>parseInt(itemline.order)===this.props.order.user)
     let price = 0
     let quantity = 0
-    itemList.forEach((item)=>{
+    Listitem.forEach((item)=>{
       quantity+=item.quantity
-      price+=this.props.products.find((product)=>parseInt(item.product)==product.id).price*item.quantity
+      price+=this.props.admin.product.find((product)=>parseInt(item.product)===product.id).price*item.quantity
     })
     return(
       <li className="white">
@@ -55,19 +46,10 @@ class OrderItem extends Component {
             </Col>
           </Row>
           <Row>
-            <Col s={12} m={1}></Col>
-            <Col s={12} m={7}>
-              <Row>
-                Address :
-              </Row>
-              <Row>
-                {address.address_number} {address.village} {address.road} {address.sub_distinct} {address.distinct} {address.province} {address.country} {address.zipcode}
-              </Row>
-            </Col>
-            <Col s={12} m={4}>
-              {this.props.order.is_shipped?
-                <div>{this.props.order.postal_track}</div>:<div></div>
-              }
+            <Col s={12} m={8}>
+              Address :&nbsp;&nbsp;
+              {this.getAddressbyID(this.props.order.user).address_number} {this.getAddressbyID(this.props.order.user).village} {this.getAddressbyID(this.props.order.user).road} {this.getAddressbyID(this.props.order.user).sub_distinct} {this.getAddressbyID(this.props.order.user).distinct}
+              {this.getAddressbyID(this.props.order.user).province} {this.getAddressbyID(this.props.order.user).country} {this.getAddressbyID(this.props.order.user).zipcode}
             </Col>
           </Row>
           <Row>
@@ -79,12 +61,12 @@ class OrderItem extends Component {
                   <th data-field="price">Price</th>
                 </tr>
                 <tbody>
-                  {itemList.map(
+                  {this.props.admin.item_line.map(
                     (item)=>(
                       <tr>
-                        <td>{this.props.products.find((product)=>parseInt(item.product)==product.id).name}</td>
+                        <td>{this.props.admin.product.find((product)=>parseInt(item.product)==product.id).name}</td>
                         <td>{item.quantity}</td>
-                        <td>{(this.props.products.find((product)=>parseInt(item.product)==product.id).price)*item.quantity}</td>
+                        <td>{(this.props.admin.product.find((product)=>parseInt(item.product)==product.id).price)*item.quantity}</td>
                       </tr>
                     )
                   )
