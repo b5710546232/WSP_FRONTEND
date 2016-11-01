9365a60fdfa16dbb85ed14803c5ca303a1101819
b5710546232 <nattapat.su@ku.th>
2016-11-01 20:36:47 +0700

    admin user

---
+++ test/reducers/admin.spec.js
@@ -0,0 +1,101 @@
+// LOAD_ADMIN_USER_SUCCESS
+// LOAD_ADMIN_ADDRESS_SUCCESS
+// LOAD_ADMIN_PRODUCT_SUCCESS
+// LOAD_ADMIN_CATEGORY_SUCCESS
+// LOAD_ADMIN_ORDER_SUCCESS
+// LOAD_ADMIN_ORDER_SUCCESS
+// LOAD_ADMIN_ITEMLINE_SUCCESS
+// LOAD_ADMIN_METHOD_SUCCESS
+import { expect } from '../test-helper'
+import reducer from '../..//src/app/reducers/admin'
+
+
+describe('Admin Reducer', () => {
+  let currentState
+
+  beforeEach(() => {
+    currentState = {
+      user : [],
+      address : [],
+      product : [],
+      category : [],
+      order : [],
+      item_line : [],
+      method : []
+    }
+  })
+
+  it('should return correct initial state', () => {
+    const expectedState = {
+      user : [],
+      address : [],
+      product : [],
+      category : [],
+      order : [],
+      item_line : [],
+      method : []
+    }
+
+    expect(reducer(undefined, {})).to.deep.equal(expectedState)
+  })
+  describe('Admin load user', () => {
+    it('should loading the user data if request success', () => {
+      const action = {
+        type: 'LOAD_ADMIN_USER_SUCCESS',
+        payload:
+          [
+            {
+              "email": "safe@com.com",
+              "username": "safe",
+              "first_name": "safe",
+              "last_name": "safe",
+              "id": 1,
+              "is_active": true,
+              "is_staff": false
+            },
+            {
+              "email": "s@s.com",
+              "username": "aaaa",
+              "first_name": "s",
+              "last_name": "s",
+              "id": 4,
+              "is_active": true,
+              "is_staff": false
+            }
+          ]
+      }
+      const expectedState = {
+        user : [
+          {
+            "email": "safe@com.com",
+            "username": "safe",
+            "first_name": "safe",
+            "last_name": "safe",
+            "id": 1,
+            "is_active": true,
+            "is_staff": false
+          },
+          {
+            "email": "s@s.com",
+            "username": "aaaa",
+            "first_name": "s",
+            "last_name": "s",
+            "id": 4,
+            "is_active": true,
+            "is_staff": false
+          }
+        ],
+        address : [],
+        product : [],
+        category : [],
+        order : [],
+        item_line : [],
+        method : []
+      }
+      const nextState = reducer(currentState, action)
+      reducer(currentState, action)
+      expect(nextState).to.deep.equal(expectedState)
+    })
+  })
+
+})
