39b04a96b1d144b66a2d3c4b346c91e16b2f074f
b5710546232 <nattapat.su@ku.th>
2016-11-02 11:06:49 +0700

    package

--- webpack.config.js
+++ webpack.config.js
@@ -41,13 +41,17 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = 'eval'
+  module.exports.devtool = '#eval'
   console.log('production_safe');
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
+    new webpack.optimize.UglifyJsPlugin({
+                sourceMap: false,
+                minimize: true,
+                compress: {
+                    drop_debugger: true,
+                    warnings: false,
+                    drop_console: true
+                }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
