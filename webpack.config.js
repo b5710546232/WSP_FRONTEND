a73f38eb7985f48d9d763393df86faaa90a2497a
b5710546232 <nattapat.su@ku.th>
2016-11-02 10:58:05 +0700

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
       compress: {
         warnings: false
       }
