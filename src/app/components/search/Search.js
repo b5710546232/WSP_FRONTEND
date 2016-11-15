86af8a1f69445af83b5b305f793b3ed380a48828
paiizz <wanchanapon.t@ku.th>
2016-11-15 23:06:14 +0700

    :rotating_light:can keet word that input

--- src/app/components/search/Search.js
+++ src/app/components/search/Search.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react'
 import SearchBar from 'react-search-bar'
 import '../../../assets/scss/search.scss'
-export default class Search extends Component{
+import {Icon,Input,NavItem,Row,Navbar,Col} from 'react-materialize'
+import {connect} from 'react-redux'
+class Search extends Component{
+  constructor(){
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch(event){
+    this.setState({search: event.target.value.substr(0,20)});
+    console.log(this.state.search,"test search");
+  }
 
   render() {
+    let filterSearch = this.props.categories.filter(
+      (category) => {
+        return category.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== 1;
+      }
+    );
     return (
-      <div>
-        <nav>
-        <div className="nav-wrapper">
-        <form>
-        <div center className="input-field">
-          <input id="search" type="search" required placeholder="Search..."/>
-          <label for="search"></label>
+        <div className="col s12 ">
+          <div className="filter white  z-depth-2 ">
+            <Row>
+            <Col s={12}>
+              <h2 className="container ">
+                <input  id="search"
+                        type="search"
+                        required placeholder="Search..."
+                        value={this.state.search}
+                        onChange={this.updateSearch.bind(this)}/>
+              </h2>
+            </Col>
+            {/* <Col s={1}>
+              <Icon>search</Icon>
+            </Col> */}
+            </Row>
+          </div>
         </div>
-        </form>
-        </div>
-        </nav>
-      </div>
+
     )
   }
 }
+const mapStateToProps = (state) => {
+  return state
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // onLoadCategoryList: () => {
+    //   dispatch(loadCategoryList())
+    // },
+    // addFilter: (filters) => {
+    //   dispatch(addFilter(filters))
+    // },
+    // removeFilter: (filters) => {
+    //   dispatch(removeFilter(filters))
+    // }
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Search)
