15d811c5d19701715e1a00ce6a442e3b4935eba1
b5710546232 <nattapat.su@ku.th>
2016-11-17 11:46:58 +0700

    cavas :bug:

--- src/app/components/design/Design.js
+++ src/app/components/design/Design.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react'
 import ParallaxSection from '../home/section/ParallaxSection'
-import {PIXI} from 'pixi'
+// import PIXI from 'pixi'
 export default class Design extends Component {
   initPixi(){
-    console.log('created');
-    // let renderer = PIXI.autoDetectRenderer(800, 600,document.getElementById('design-canvas'))
-    // renderer.backgroundColor = 0x000000;
-
+    console.log('pixi','created');
+    let renderer = PIXI.autoDetectRenderer(800, 600)
+    renderer.backgroundColor = 	0xb0bdd2
+    document.getElementById('design-container').appendChild(renderer.view)
     // create the root of the scene graph
-    // var stage = new PIXI.Container();
+    var stage =  new PIXI.Container();
 
     // create a texture from an image path
-    // var texture = PIXI.Texture.fromImage('required/assets/basics/bunny.png');
 
     // create a new Sprite using the texture
-    // var bunny = new PIXI.Sprite(texture);
+    if(!texture){
+      var texture = PIXI.Texture.fromImage("required/logo.png");
+    }
+    var bunny = new PIXI.Sprite(texture);
 
     // center the sprite's anchor point
-    // bunny.anchor.x = 0.5;
-    // bunny.anchor.y = 0.5;
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
 
     // move the sprite to the center of the screen
-    // bunny.position.x = 200;
-    // bunny.position.y = 150;
+    bunny.position.x = 200;
+    bunny.position.y = 150;
 
-    // stage.addChild(bunny);
-    // renderer.render(stage)
+    stage.addChild(bunny);
+    renderer.render(stage)
   }
   componentDidMount(){
+
     this.initPixi()
+    this.forceUpdate()
   }
   render() {
     let canvasStyle={
@@ -38,8 +42,8 @@ export default class Design extends Component {
     return (
       <ParallaxSection
         background="src/assets/media/images/2.jpg">
-        <div className="container">
-          <canvas id="design-canvas" style={canvasStyle}></canvas>
+        <div id="design-container" className="container">
+          {/* <canvas id="design-canvas" style={canvasStyle}></canvas> */}
         </div>
       </ParallaxSection>
     )
