deb117a49d73bed6fcffc037fa60016013dfc262
paiizz <wanchanapon.t@ku.th>
2016-11-20 20:26:27 +0700

    install chartjs

--- src/app/components/admin/statistic/StatisticBody.js
+++ src/app/components/admin/statistic/StatisticBody.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import '../../../../assets/scss/admin.scss'
 import {loadProduct,loadCategory,loadMoneyProduct,loadUserPayment,loadUserOrder,loadUserShiping,loadAddress} from '../../../actions/StatisticAction'
-
+import {LineChart} from 'react-chartjs'
 
 class StatisticBody extends Component {
   componentDidMount(){
@@ -43,7 +43,9 @@ class StatisticBody extends Component {
 
           </table>
         </div>
+        <div><LineChart data={this.props.statistic.product} /></div>
       </li>
+
     )
   }
 }
