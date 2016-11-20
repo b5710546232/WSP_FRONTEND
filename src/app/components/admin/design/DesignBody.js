6f462e33c9b1e0737bab29766beebf55a53bc390
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-20 21:10:53 +0700

    Load to design admin

--- src/app/components/admin/design/DesignBody.js
+++ src/app/components/admin/design/DesignBody.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import '../../../../assets/scss/admin.scss'
-
+import {loadDesign,loadBottle,loadLogo,loadBanner} from '../../../actions/AdminAction'
 class DesignBody extends Component {
   componentDidMount(){
+    this.props.loadDesign(localStorage.token)
+    this.props.loadBottle(localStorage.token)
+    this.props.loadBanner(localStorage.token)
+    this.props.loadLogo(localStorage.token)
+
     $(document).ready(function(){
       $('.collapsible').collapsible({
         accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
       });
     });
   }
-  shouldComponentUpdate(nextProps){
-    return this.props.admin.user !== nextProps
-  }
-  componentDidUpdate(){
 
-
-  }
   render() {
     return(
       <li>
@@ -34,6 +33,19 @@ const mapStateToProps = (state) => {
   return state
 }
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return {
+    loadDesign: (token)=>(
+      dispatch(loadDesign(token))
+    ),
+    loadBanner: (token)=>(
+      dispatch(loadBanner(token))
+    ),
+    loadBottle: (token)=>(
+      dispatch(loadBottle(token))
+    ),
+    loadLogo: (token)=>(
+      dispatch(loadLogo(token))
+    )
+  }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(DesignBody)
