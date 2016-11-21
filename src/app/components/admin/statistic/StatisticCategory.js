52360c187660630de654bc4b2777944af3b72305
paiizz <wanchanapon.t@ku.th>
2016-11-21 12:40:56 +0700

    fix category

--- src/app/components/admin/statistic/StatisticCategory.js
+++ src/app/components/admin/statistic/StatisticCategory.js
@@ -10,12 +10,19 @@ class StatisticCategory extends Component {
     this.props.loadCategory()
   }
 
+  componentWillUpdate(){
+    this.statistic = this.props.statistic.category
+    for(let i=0;i<this.statistic.length;i++){
+        this.statistic[i].category_name = this.props.admin.category.find((category)=>(category.id==this.statistic[i].category)).name
+    }
+  }
+
 
   render() {
     return(
-      <LineChart width={780} height={300} data={this.props.statistic.category}
+      <LineChart width={780} height={300} data={this.statistic}
             margin={{top: 20, right: 100, left: 10, bottom: 5}}>
-       <XAxis label="category name" dataKey="category"/>
+       <XAxis label="category name" dataKey="category_name"/>
        <YAxis label="amount"/>
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
