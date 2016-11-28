f542fce429e10fcf07e8092fb6329f7596594f36
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-28 18:10:50 +0700

    Fix bug on NavBar

--- src/app/components/store/sidebar/sidebar.js
+++ src/app/components/store/sidebar/sidebar.js
@@ -3,16 +3,19 @@ import Search from '../search/Search'
 import Sort from '../sort/Sort'
 import Filter from '../filter/Filter'
 export default class Sidebar extends Component{
+  componenDidMount(){
+    $(".button-collapse").sideNav();
+  }
   render(){
     return(
       <div>
         <ul id="slide-out" className="side-nav">
-        {/* <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li> */}
-        <li><Sort/></li>
-        <li><Search/></li>
-        <li><Filter/></li>
-        {/* <li><Sort/></li> */}
-        {/* <li><a href="#!">Second Link</a></li> */}
+          {/* <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li> */}
+          <li><Sort/></li>
+          <li><Search/></li>
+          <li><Filter/></li>
+          {/* <li><Sort/></li> */}
+          {/* <li><a href="#!">Second Link</a></li> */}
         {/* <li><a className="waves-effect" href="#!">Third Link With Waves</a></li> */}
         </ul>
       <a data-activates="slide-out" className="waves-effect waves-light btn button-collapse">Sort/Search/Filter</a>
