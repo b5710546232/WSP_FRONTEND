c502d77fee26d57b79b7b776f8c915eb30d751b0
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-28 20:35:15 +0700

    Fix bug on Update Sidebar

--- src/app/components/store/sidebar/sidebar.js
+++ src/app/components/store/sidebar/sidebar.js
@@ -3,22 +3,22 @@ import Search from '../search/Search'
 import Sort from '../sort/Sort'
 import Filter from '../filter/Filter'
 export default class Sidebar extends Component{
-  componenDidMount(){
-    $(".button-collapse").sideNav();
+  componentDidMount(){
+    $("#filterSide").sideNav();
   }
   render(){
     return(
       <div>
-        <ul id="slide-out" className="side-nav white">
+        <ul id="slide-out-filter" className="side-nav white">
           {/* <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li> */}
           <li><Sort/></li>
           <li><Search/></li>
           <li><Filter/></li>
           {/* <li><Sort/></li> */}
           {/* <li><a href="#!">Second Link</a></li> */}
-        {/* <li><a className="waves-effect" href="#!">Third Link With Waves</a></li> */}
+          {/* <li><a className="waves-effect" href="#!">Third Link With Waves</a></li> */}
         </ul>
-      <a data-activates="slide-out" className="waves-effect waves-light btn button-collapse">Sort/Search/Filter</a>
+        <a data-activates="slide-out-filter" id="filterSide" className="waves-effect waves-light btn button-collapse">Sort/Search/Filter</a>
       </div>
     )
   }
