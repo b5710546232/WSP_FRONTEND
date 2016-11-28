85e0eec4928d54e9bb186035b15922f1600851f5
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-28 19:22:35 +0700

    Show name on sort

--- src/app/components/store/sort/Sort.js
+++ src/app/components/store/sort/Sort.js
@@ -4,28 +4,38 @@ import {connect} from 'react-redux'
 import {sortNone,sortPriceLow,sortPriceHigh,sortNameA,sortNameZ} from '../../../actions/SortAction'
 import {loadProductList} from '../../../actions/ProductAction'
 class Sort extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      name : "Sort by A-Z"
+    }
+  }
   componentWillMount(){
-    this.sortByNone()
+    this.sortByNameA()
   }
   sortByNone(){
     this.props.sortNone(this.props.search.search_product)
   }
   sortByNameA(){
     this.props.sortNameA(this.props.search.search_product)
+    this.setState({name : "Sort by A-Z"})
   }
   sortByNameZ(){
     this.props.sortNameZ(this.props.search.search_product)
+    this.setState({name : "Sort by Z-A"})
   }
   sortByPriceLow(){
     this.props.sortPriceLow(this.props.search.search_product)
+    this.setState({name : "Sort by Price Low-High"})
   }
   sortByPriceHigh(){
     this.props.sortPriceHigh(this.props.search.search_product)
+    this.setState({name : "Sort by Price High-Low"})
   }
   render(){
     return(
       <div className="sort_container">
-         <a className='dropdown-button btn' data-activates='dropdown1'>Sort by</a>
+        <a className='dropdown-button btn' data-activates='dropdown1'>{this.state.name}</a>
          <ul id='dropdown1' className='dropdown-content'>
            <li><span onClick={()=>this.sortByNameA()}>Name: A-Z</span></li>
            <li><span onClick={()=>this.sortByNameZ()}>Name: Z-A</span></li>
